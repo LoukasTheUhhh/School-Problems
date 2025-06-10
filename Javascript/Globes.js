fdb2a57ba988f52594ae8b31ecabbc849e9e42db
Daved <Cazamirloucas@gmail.com>
2025-06-10 11:24:53 +0300

    Globes problem fixed in Javascript.

--- Javascript/Globes.js
+++ Javascript/Globes.js
@@ -3,21 +3,17 @@ There is (A) White Globes,There Is Two Times More Red Globes,And 3 Green Globes 
 How many globes are there in total?
 (Also,there cant be more than or 1000 white globes,or less than or 2 white globes.)
 */
-let uhh = input("How many White Globes should there be?\n-");
-let whiteGlobes = int(uhh);
-if (wG >= 1000) {
-  console.log("Error: Invalid White Globe Number(Too Much).");
-  process.exit();
-} else if (wG <= 2) {
-  console.log("Error: Invalid White Globe Number(Not Enough).")
-} else {
-  console.log("There are "+str(whiteGlobes)+" White Globes.");
-  let redGlobez = wG * 2;
-  console.log("There are "+str(redGlobes)+" Red Globes.");
-  let greenGlobes = rG - 3;
-  console.log("There are "+str(greenGlobes)+" Green Globes.");
-  let totalGlobes = whiteGlobes + redGlobes + greenGlobes;
-  console.log("There are "+str(totalGlobes)+" Total Globes.");
+let whiteGlobes = parseInt(prompt("How many White Globes should there be?\n-"));
+if (whiteGlobes >= 1000) {
+  console.error("Error: Invalid White Globe Number(Too Much).");
+} else if (whiteGlobes <= 2) {
+  console.error("Error: Invalid White Globe Number(Not Enough).")
 }
-process.exit();
+console.log("There are "+whiteGlobes.toString()+" White Globes.");
+let redGlobes = wG * 2;
+console.log("There are "+redGlobez.toString()+" Red Globes.");
+let greenGlobes = rG - 3;
+console.log("There are "+greenGlobes.toString()+" Green Globes.");
+let totalGlobes = whiteGlobes + redGlobes + greenGlobes;
+console.log("There are "+totalGlobes.toString()+" Total Globes.");
   
